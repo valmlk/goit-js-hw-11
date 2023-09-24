eba1275bda19d8256ccb01cda780f093b89db664
valmlk <valyushka.malko@gmail.com>
2023-09-24 10:56:00 +0200

    upd

--- src/js/api.js
+++ src/js/api.js
@@ -4,6 +4,7 @@ const API_KEY = "39603904-54e1726f85aaa96ca931f61d5";
 axios.defaults.baseURL = `https://pixabay.com/api/`;
 
 async function fetch(value, page, perPage) {
+  if( value === "") return [];
   return await axios.get(`?key=${API_KEY}&q=${value}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
 }
 
